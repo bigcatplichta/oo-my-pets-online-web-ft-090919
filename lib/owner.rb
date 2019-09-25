ee2dc83bd460ad473baf9e27bd343391d48d08d3
Matt Plichta <bigcatplichta@gmail.com>
2019-09-25 17:28:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,6 +58,6 @@ class Owner
   end 
   
   def list_pets
-    print "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    return "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end 
 end
