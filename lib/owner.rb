b200b270c17a9cc481e5dcc44f413ca1131069ec
Matt Plichta <bigcatplichta@gmail.com>
2019-09-25 17:08:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader :name, :species
+  attr_writer
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
+  end 
+  
+  def say_species
+    return "I am a #{@species}."
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.count
+  end 
+  
+  def self.reset_all
+    @@all.clear 
+  end 
+  
+  def cats 
+    @cats
+  end 
+  
+  def buy_cat
+    
+  end 
+  
+  def buy_dog
+    
+  end 
 end
